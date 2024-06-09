c9d4ccbc5fb84a4f1a5d64145062794ccc29d968
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-06-09 13:00:46 +0100

    feat: implement role middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -11,14 +11,14 @@ func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.JSON(401, gin.H{"error": "Authorization header is required"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
 			c.Abort()
 			return
 		}
 
 		parts := strings.Split(authHeader, " ") // Split the Authorization header by space (e.g. "Bearer <token>" => ["Bearer", "<token>"])
 		if len(parts) != 2 || parts[0] != "Bearer" {
-			c.JSON(401, gin.H{"error": "Authorization header must be in the format 'Bearer <token>'"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header must be in the format 'Bearer <token>'"})
 			c.Abort()
 			return
 		}
--- internal/middleware/role.go
+++ internal/middleware/role.go
@@ -1,5 +1,39 @@
 package middleware
 
-func Role() {
-	// TODO: Implement Role middleware
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Role(role string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		claims, exists := c.Get("claims")
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "claims not found in context"})
+			c.Abort()
+			return
+		}
+
+		claimsMap, ok := claims.(map[string]interface{})
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "claims is not a map"})
+			c.Abort()
+			return
+		}
+
+		roleClaim, exists := claimsMap["role"]
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "role claim not found in claims"})
+			c.Abort()
+			return
+		}
+
+		if roleClaim != role {
+			c.JSON(http.StatusForbidden, gin.H{"error": "user does not have the required role"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
 }
